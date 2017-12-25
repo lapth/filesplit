16fcd04005aa2d3711320765fc75e6958c4e12f2
Lap Tran <lapth82@gmail.com>
2017-12-25 01:34:36 -0800

    test

---
+++ FileSplit/FileSplit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSplit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
